6c0bf446e18422cfb6230026bcfdbfea3966190e
Valery Bugakov <skymk1@gmail.com>
2016-03-30 23:42:35 +0400

    (install.sh): disabled auto switch to zsh

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ git clone $DOTFILES_REPO $DOTFILES_PATH
 find ~/dotfiles/dots -name '.*' | xargs -I % ln -sfv % ~
 
 cat $DOTFILES_PATH/ohmyzsh.sh | sh
-sudo echo `which bash` >> /etc/shells
-chsh -s /bin/zsh
+# sudo echo `which bash` >> /etc/shells
+# chsh -s /bin/zsh
 
 # mkdir -p $HOME/bin
 # curl -o $HOME/vim.tar.bz2 ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
